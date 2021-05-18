f0adfea02079d855cc21783d7b5381369d201efc
JeffReeves <jeff@binary.run>
2021-05-17 23:02:07 -0700

    Add discord.py demo code

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+
+class MyClient(discord.client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+client = MyClient()
+client.run('token')
