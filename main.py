d4947bb3ed995f6917ff2f8e7323dc899f085e83
JeffReeves <jeff@binary.run>
2021-05-18 00:11:06 -0700

    Add dm_user for testing DMs

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import discord
 # get token from environment variable
 # TODO: 
 #   - prompt user for token, if not provided as env variable
-token = os.environ.get('DISCORD_TOKEN')
+token   = os.environ.get('DISCORD_TOKEN')
+dm_user = os.environ.get('DISCORD_DM_USER')
 
 # add intents to get users by id
 intents         = discord.Intents.default()
@@ -14,6 +15,8 @@ client          = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    user = await client.get_user_info(dm_user)
+    await client.send_message(user, "Ready to watch your laundry!")
 
 @client.event
 async def on_message(message):
@@ -31,13 +34,20 @@ async def on_message(message):
         print(client.user)
         await message.channel.send('Hello!')
 
+        # get the user by id
         user = client.get_user(message.author.id)
         print('[DEBUG] user:')
         print(user)
         await user.send('👀 I see you')
+        # alternative to send a DM to the message's author
         #print('[DEBUG] message.author:')
         #print(message.author)
         #await message.author.send('👀 I see you')
+
+@client.command(pass_context=True)
+async def dm(ctx):
+    user=await client.get_user_info("User's ID here")
+    await client.send_message(user, "Your message goes here")
 
 if token:
     client.run(token)
