0f5b7170769102968d82b02bb83fd84eccc28882
JeffReeves <jeff@binary.run>
2021-05-18 00:21:51 -0700

    Change fetch_user to get_user

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ client          = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    user = await client.fetch_user(dm_user)
+    #user = await client.fetch_user(dm_user)
+    user = await client.get_user(dm_user)
     print('[DEBUG] user:')
     print(user)
     await user.send("Ready to watch your laundry!")
