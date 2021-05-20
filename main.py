784750d210a1e6677188002984c74e53170aa59e
JeffReeves <jeff@binary.run>
2021-05-20 16:34:59 -0700

    Add argparse
    Cleaned up sections and added argparse for command line parameters

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ purpose: Discord Bot that sends laundry status messages.
 author: Jeff Reeves
 """
 
+#==[ IMPORTS ]=============================================================================================================================
+
+from pprint import pprint
 import os
 import traceback
+import base64
+import argparse
 import getpass
 import discord
 
-debug  = True
-prefix = '!'
 
-# use environment variable or user input
-token   = os.environ.get('DISCORD_TOKEN')   or getpass.getpass('Token: ')
-dm_user = os.environ.get('DISCORD_DM_USER') or input('User ID: ')
+#==[ CONFIG ]==============================================================================================================================
+
+# globals
+debug    = True
+prefix   = '!'
+watchers = []
 
 # create client 
 # NOTE: intents are needed to get users by id, this must be set in 
@@ -26,11 +32,14 @@ dm_user = os.environ.get('DISCORD_DM_USER') or input('User ID: ')
 #       Application -> Bot -> SERVER MEMBERS INTENT (ON)
 intents         = discord.Intents.default()
 intents.members = True
-client          = discord.Client(intents=intents)
+client          = discord.Client(intents = intents)
+
+
+#==[ FUNCTIONS ]===========================================================================================================================
 
 # get user by ID
-async def get_user_by_id(dm_user):
-    user = await client.fetch_user(dm_user)
+async def get_user_by_id(watcher):
+    user = await client.fetch_user(watcher)
     if debug:
         print('[DEBUG] user:')
         print(user)
@@ -47,7 +56,7 @@ async def on_ready():
     # fetch the user ID for the user to DM
     # TODO:
     #   - create array of users to DM
-    user = await get_user_by_id(dm_user)
+    user = await get_user_by_id(watcher)
     await send_dm(user)
 
 # DISCONNECT
@@ -64,7 +73,7 @@ async def on_error(event, *args, **kwargs):
         print(message)
     print(traceback.format_exc())
     #logging.warning(traceback.format_exc()) #logs the error
-    user = await client.fetch_user(dm_user)
+    user = await client.fetch_user(watcher)
     if debug:
         print('[DEBUG] user:')
         print(user)
@@ -108,9 +117,76 @@ async def on_message(message):
             #user = await get_user_by_id(message.author.id)
             #await send_dm(user, message = '👀 I see you 👍')
 
-if token:
-    client.run(token)
-else:
-    print('[ERROR] No token provided')
-    print('[HELP] Set DISCORD_TOKEN variable and run again')
-    print('[EXAMPLE] export DISCORD_TOKEN=\'mycooltoken\'')
+#==[ MAIN ]================================================================================================================================
+
+def main(args):
+
+    if args.debug:
+        print('[DEBUG] Main called')
+        print('[DEBUG] All arguments passed to script:')
+        pprint(args)
+
+    # try to get values from environment variables
+    token   = os.environ.get('DISCORD_TOKEN')
+    watcher = os.environ.get('DISCORD_WATCHER')
+
+    if token:
+        client.run(token)
+    else:
+        print('[ERROR] No token provided')
+        print('[HELP] Set DISCORD_TOKEN variable and run again')
+        print('[EXAMPLE] export DISCORD_TOKEN=\'mycooltoken\'')
+
+    return
+
+#==[ COMMAND LINE ]========================================================================================================================
+
+if __name__ == "__main__":
+
+    # custom types for argparse
+    def decode_base64_token(base64_token):
+        return base64.base64decode(base64_token)
+
+    # create parser
+    parser = argparse.ArgumentParser()
+
+    # create groups of arguments
+    group_tokens    = parser.add_mutually_exclusive_group(required = True, type = str)
+    group_watchers  = parser.add_mutually_exclusive_group(required = True, type = str)
+
+    # add arguments to parser
+    parser.add_argument('-d', 
+                        '--debug', 
+                        dest    = 'debug', 
+                        action  = 'store_true', 
+                        help    = 'Debug Mode')
+
+    group_tokens.add_argument('-t', 
+                              '--token',
+                              dest      = 'token',
+                              help      = 'Bot Token')
+
+    group_tokens.add_argument('-b', 
+                              '--base64_token',
+                              dest      = 'token',
+                              type      = decode_base64_token, 
+                              help      = 'Bot Token (base64)')
+
+    group_watchers.add_argument('-w', 
+                                '--watcher', 
+                                action  = 'append', 
+                                help    = 'User ID of Watcher')
+
+    group_watchers.add_argument('--watchers', 
+                                nargs   = '+', 
+                                help    = 'User IDs for Watchers (space separated list)')
+
+    # parse argument
+    args = parser.parse_args()
+
+    # decode encrypted token
+    if not args.token and not args.base64_token: 
+        args.token = getpass('Bot Token: ')
+
+    # pass all arguments as a dictionary to the main function
+    main(args)
