a2749a0451fd5a07b0fbbdcaab47ebbefdf58ccd
JeffReeves <jeff@binary.run>
2021-05-20 17:11:55 -0700

    Move functions into main for testing

--- main.py
+++ main.py
@@ -17,106 +17,6 @@ import argparse
 import getpass
 import discord
 
-
-#==[ CONFIG ]==============================================================================================================================
-
-# globals
-debug    = True
-prefix   = '!'
-watchers = []
-
-# create client 
-# NOTE: intents are needed to get users by id, this must be set in 
-#   the Discord Dev Center:
-#       https://discord.com/developers/applications/ -> 
-#       Application -> Bot -> SERVER MEMBERS INTENT (ON)
-intents         = discord.Intents.default()
-intents.members = True
-client          = discord.Client(intents = intents)
-
-
-#==[ FUNCTIONS ]===========================================================================================================================
-
-# get user by ID
-async def get_user_by_id(watcher):
-    user = await client.fetch_user(watcher)
-    if debug:
-        print('[DEBUG] user:')
-        print(user)
-    return user
-
-# send DM
-async def send_dm(user, message = 'Sending you a message'):
-    await user.send(message)
-
-# READY
-@client.event
-async def on_ready():
-    print('[INFO] Bot {0.user} is ready'.format(client))
-    # fetch the user ID for the user to DM
-    # TODO:
-    #   - create array of users to DM
-    user = await get_user_by_id(watcher)
-    await send_dm(user)
-
-# DISCONNECT
-@client.event
-async def on_disconnect():
-    print('[WARN] Disconnected from Discord')
-
-# ERROR
-@client.event
-async def on_error(event, *args, **kwargs):
-    message = args[0] #Gets the message object
-    if debug:
-        print('[DEBUG] message:')
-        print(message)
-    print(traceback.format_exc())
-    #logging.warning(traceback.format_exc()) #logs the error
-    user = await client.fetch_user(watcher)
-    if debug:
-        print('[DEBUG] user:')
-        print(user)
-    await user.send('Encountered an error...')
-
-# MESSAGE
-@client.event
-async def on_message(message):
-
-    # return if the message is from the bot
-    if message.author == client.user:
-        return
-
-    # check for command prefix
-    if message.content.startswith(prefix):
-
-        # debugging
-        if debug:
-            print('[DEBUG] message:')
-            print(message)
-            print('[DEBUG] message.author.id:')
-            print(message.author.id)
-            print('[DEBUG] client:')
-            print(client)
-            print('[DEBUG] client.user:')
-            print(client.user)
-            print('[DEBUG] message.channel:')
-            print(message.channel)
-            print('[DEBUG] message.channel.type:')
-            print(message.channel.type)
-
-        # send a message back to the channel
-        await message.channel.send('Hello!')
-
-        # check if private message
-        if message.channel.type == discord.ChannelType.private:
-            if debug:
-                print('[DEBUG] Received a private message')
-            # send PM to the author
-            await message.author.send('👀 I see you 👍')
-            #user = await get_user_by_id(message.author.id)
-            #await send_dm(user, message = '👀 I see you 👍')
-
 #==[ MAIN ]================================================================================================================================
 
 def main(args):
@@ -126,9 +26,104 @@ def main(args):
         print('[DEBUG] All arguments passed to script:')
         pprint(args)
 
+    # create client 
+    # NOTE: intents are needed to get users by id, this must be set in 
+    #   the Discord Dev Center:
+    #       https://discord.com/developers/applications/ -> 
+    #       Application -> Bot -> SERVER MEMBERS INTENT (ON)
+    intents         = discord.Intents.default()
+    intents.members = True
+    client          = discord.Client(intents = intents)
+
+    # config 
+    debug    = True
+    prefix   = '!'
+    watchers = []
+
     # try to get values from environment variables
-    token   = os.environ.get('DISCORD_TOKEN')
-    watcher = os.environ.get('DISCORD_WATCHER')
+    token    = os.environ.get('DISCORD_TOKEN')
+    watcher  = os.environ.get('DISCORD_WATCHER')
+    
+
+    # get user by ID
+    async def get_user_by_id(watcher):
+        user = await client.fetch_user(watcher)
+        if debug:
+            print('[DEBUG] user:')
+            print(user)
+        return user
+
+    # send DM
+    async def send_dm(user, message = 'Sending you a message'):
+        await user.send(message)
+
+    # READY
+    @client.event
+    async def on_ready():
+        print('[INFO] Bot {0.user} is ready'.format(client))
+        # fetch the user ID for the user to DM
+        # TODO:
+        #   - create array of users to DM
+        user = await get_user_by_id(watcher)
+        await send_dm(user)
+
+    # DISCONNECT
+    @client.event
+    async def on_disconnect():
+        print('[WARN] Disconnected from Discord')
+
+    # ERROR
+    @client.event
+    async def on_error(event, *args, **kwargs):
+        message = args[0] #Gets the message object
+        if debug:
+            print('[DEBUG] message:')
+            print(message)
+        print(traceback.format_exc())
+        #logging.warning(traceback.format_exc()) #logs the error
+        user = await client.fetch_user(watcher)
+        if debug:
+            print('[DEBUG] user:')
+            print(user)
+        await user.send('Encountered an error...')
+
+    # MESSAGE
+    @client.event
+    async def on_message(message):
+
+        # return if the message is from the bot
+        if message.author == client.user:
+            return
+
+        # check for command prefix
+        if message.content.startswith(prefix):
+
+            # debugging
+            if debug:
+                print('[DEBUG] message:')
+                print(message)
+                print('[DEBUG] message.author.id:')
+                print(message.author.id)
+                print('[DEBUG] client:')
+                print(client)
+                print('[DEBUG] client.user:')
+                print(client.user)
+                print('[DEBUG] message.channel:')
+                print(message.channel)
+                print('[DEBUG] message.channel.type:')
+                print(message.channel.type)
+
+            # send a message back to the channel
+            await message.channel.send('Hello!')
+
+            # check if private message
+            if message.channel.type == discord.ChannelType.private:
+                if debug:
+                    print('[DEBUG] Received a private message')
+                # send PM to the author
+                await message.author.send('👀 I see you 👍')
+                #user = await get_user_by_id(message.author.id)
+                #await send_dm(user, message = '👀 I see you 👍')
 
     if token:
         client.run(token)
@@ -138,6 +133,7 @@ def main(args):
         print('[EXAMPLE] export DISCORD_TOKEN=\'mycooltoken\'')
 
     return
+
 
 #==[ COMMAND LINE ]========================================================================================================================
 
